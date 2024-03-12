a6378b3e28c25f2a59cf2f74d33508cb4e19cd37
runeunhjem <runeunhjem@gmail.com>
2024-03-12 12:11:36 +0100

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}", // Adjust this line if you're using TypeScript
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+
