3e9c8a773a774287ea04c5549361bd527d49de9b
runeunhjem <runeunhjem@gmail.com>
2024-03-23 13:08:15 +0100

    Updates main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import "./styles/global.css";
 const isDarkMode = useStore.getState().isDarkMode;
 document.body.setAttribute("data-theme", isDarkMode ? "dark" : "light");
 // console.log(import.meta.env.VITE_API_KEY);
-console.log(import.meta.env);
+// console.log(import.meta.env);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   // <React.StrictMode>
